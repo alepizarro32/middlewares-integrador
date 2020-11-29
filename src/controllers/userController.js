6d413460f8e00c80e64565975aa0cea5615841fb
alepizarro32 <alepizarro32@gmail.com>
2020-11-28 21:21:34 -0300

    Verificaciones y creacion de usuarios casi desarrollada

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,47 @@
+const path = require('path');
+const fs = require('fs');
+//const multer = require ('multer')
+const {check, validationResult, body} = require('express-validator'); 
+const bycrypt = require('bcrypt');
+const { fileLoader } = require('ejs');
+
+
+const usersFilePath = path.join(__dirname,'../database/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, {encoding:'utf-8'}));
+
+const writeUsers = function(array){
+    let stringUsers = JSON.stringify(array, null,1);
+    fs.writeFileSync(usersFilePath, stringUsers)
+}
+
+const newID = function(){
+    let lastID = (users[users.length-1].id)
+    return ++lastID
+}
+
 module.exports = {
     showRegister: (req, res) => {
-        // Do the magic
-        return res.send('Do the magic');
+        return res.render('user/user-register-form');
     },
-    processRegister: (req, res) => {
-        // Do the magic
-        return res.send('Do the magic');
+    processRegister: (req, res, next) => {
+        let errors = validationResult(req);
+        if (!errors.isEmpty()){
+            console.log(errors)
+            return res.render('user/user-register-form', {errors:errors.errors})
+        } else {
+            let newUser ={
+                id: newID(),
+                email:req.body.email,
+                password:bycrypt.hashSync(req.body.password, 10),
+                avatar:req.files[0].filename
+            }
+            let usersToSave = JSON.stringify([...users, newUser]);
+            console.log(usersToSave)
+            // crear una variable de objeto literal con el nuevo usuario (en la cual debemos hashear la contraseña y poner el nombre de la imagen
+            // crear un array tomando los usuarios y agregando el nuevo usuario
+    
+            // escribir en la base de datos
+            return res.send('Registro completo')};
     },
     showLogin: (req, res) => {
         // Do the magic
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,13 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const {check, validationResult, body} = require('express-validator'); 
+const fs = require('fs');
+const path = require('path')
+
+const usersFilePath = path.join(__dirname,'../database/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, {encoding:'utf-8'}));
+
+const avatarsFilePath = path.join(__dirname,'../../public/images/users');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, avatarsFilePath)
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "_" + path.basename(file.originalname, path.extname(file.originalname)) +'_' + Date.now() + path.extname(file.originalname))
+    }
+  })
+  var upload = multer({ storage: storage })
 
 const userController = require('../controllers/userController');
+const { extname } = require('path');
 
 // Muestra la vista de registro
 router.get('/register', userController.showRegister);
 
 // Procesa la vista de registro
-router.post('/register', userController.processRegister);
+router.post('/register',/*[
+    check('email').isEmail().withMessage('El email ingresado no es válido.'),
+    check('password').isLength({min:8}).withMessage('La contraseña debe tener al menos 8 caracteres.'),
+    body('retype').custom(function(value, {req}){
+        if (req.body.password == req.body.retype){
+            return true} 
+        return false
+    }).withMessage('Las contraseñas no coinciden.'),
+    body('email').custom(function(value){
+        if (users == ""){
+            users = [];
+        }else{
+            for (user of users){
+                if (user.email == value){
+                    return false
+                }
+            }
+        return true
+        }
+    }).withMessage('El email ya está en uso.')
+
+] ,*/upload.any(), userController.processRegister);
 
 // Muestra la vista de login
 router.get('/login', userController.showLogin);
@@ -20,5 +61,6 @@ router.get('/profile', userController.showProfile);
 
 // Cierra la sesión
 router.get('/logout', userController.logout);
+
 
 module.exports = router;
